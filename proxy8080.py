c570347017a2f5027c820da9c93f7d1a1ec4e55d
Junior Christofoli <34336694+juniorfdtech@users.noreply.github.com>
2018-03-21 05:52:31 -0300

    Update proxy8080.py

--- proxy8080.py
+++ proxy8080.py
@@ -11,8 +11,8 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:144'
-RESPONSE = 'HTTP/1.1 200 ~TwoSSH~\r\n\r\n'
+DEFAULT_HOST = '127.0.0.1:443'
+RESPONSE = 'HTTP/1.1 200 ~TwoVPS~\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 
@@ -169,7 +169,7 @@ class ConnectionHandler(threading.Thread):
             host = host[:i]
         else:
             if self.method=='CONNECT':
-                port = 144
+                port = 443
             else:
                 port = 8080
 
