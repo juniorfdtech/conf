9ad92983705fd6a38b1fa0d37bee172669585c9d
Junior Christofoli <34336694+juniorfdtech@users.noreply.github.com>
2018-03-22 12:39:58 -0300

    Update proxy3128.py

--- proxy3128.py
+++ proxy3128.py
@@ -11,7 +11,7 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:443'
+DEFAULT_HOST = '127.0.0.1:144'
 RESPONSE = 'HTTP/1.1 200 ~TwoVPS~\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
@@ -169,7 +169,7 @@ class ConnectionHandler(threading.Thread):
             host = host[:i]
         else:
             if self.method=='CONNECT':
-                port = 443
+                port = 144
             else:
                 port = 3128
 
